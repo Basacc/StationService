ac0251f02750e92512c7fca2ec7dcb75feba40e2
Basacc <basacc12@gmail.com>
2021-03-22 14:57:36 +0100

    Ajoutez des fichiers projet.

---
+++ StationService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
